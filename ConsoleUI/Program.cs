58f2356c980d6f0ec09c1be0fa1266fa04e5c6ea
FURKAN KOÇOĞLU <furkankocoglu@outlook.com.tr>
2023-11-18 23:32:04 +0300

    Core Layer Added. Result Interface And Classes Added. Order Entity Added. Static Messages Addes.

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -5,46 +5,20 @@ using System;
 
 namespace ConsoleUI
 {
-	//SOLID
-	//Open Closed Principle
-	class Program
-	{
-		static void Main(string[] args)
-		{
-			//Data Transformation Object
-			ProductTest();
-			//IoC 
-			//CategoryTest();
-		}
+    //SOLID
+    //Open Closed Principle
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductManager productManager = new ProductManager(new EfProductDal());
 
-		private static void CategoryTest()
-		{
-			CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
-			foreach (var category in categoryManager.GetAll())
-			{
-				Console.WriteLine(category.CategoryName);
-			}
-		}
+            foreach (var product in productManager.GetByUnitPrice(40,100))
+            {
+                Console.WriteLine(product.ProductName);
+            }
 
-		private static void ProductTest()
-		{
-			ProductManager productManager = new ProductManager(new EfProductDal());
-
-			var result = productManager.GetProductDetails();
-
-			if (result.Success == true)
-			{
-				foreach (var product in result.Data)
-				{
-					Console.WriteLine(product.ProductName + "/" + product.CategoryName);
-				}
-			}
-			else
-			{
-				Console.WriteLine(result.Message);
-			}
-
-
-		}
-	}
+            
+        }
+    }
 }
--- DataAccess/Abstract/ICategoryDal.cs
+++ DataAccess/Abstract/ICategoryDal.cs
@@ -1,4 +1,4 @@
-﻿using Core.DataAccess;
+﻿using Entities.Abstract;
 using Entities.Concrete;
 using System;
 using System.Collections.Generic;
